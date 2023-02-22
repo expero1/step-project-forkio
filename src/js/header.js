c3cc7c3ff438cab002ab97fc750022decc9068c3
Olena Hodyna <crimeaodessa2006@gmail.com>
2023-02-22 20:27:17 +0100

    add function in script and favicon

--- src/js/header.js
+++ src/js/header.js
@@ -3,27 +3,26 @@
 const burger = document.getElementById('burger');
 const navbar = document.querySelector('.navbar_menu_list');
 
-burger.addEventListener('click', () => {
-    showMenu();
-})
-
 window.addEventListener("click", (e) => {
-    let target = e.target;
-    console.log(e);
-    if (target.tagName !== "A") {
-        console.log("Hello2");
+    let trg = e.target;
+    if (burger.contains(trg)) {
         showMenu();
-        // navbar.classList.remove('inactive');
+    } else {
+    if (!navbar.contains(trg) && navbar.classList.contains('active')) {
+        burger.innerHTML = '<span>&#x2013;<br>&#x2014;<br>&#x2014;</span>';
+        navbar.classList.remove('active');
+        navbar.classList.add('inactive');
+    }
     }
 });
 
 function showMenu() {
-    if(burger.innerHTML === '<span style="font-size: 40px;">&times;</span>' || burger.innerHTML === '<span style="font-size: 40px;">×</span>') {
+    if(burger.innerHTML === '<span style="font-size: 60px;">&times;</span>' || burger.innerHTML === '<span style="font-size: 60px;">×</span>') {
         burger.innerHTML = '<span>&#x2013;<br>&#x2014;<br>&#x2014;</span>';
         navbar.classList.remove('active');
         navbar.classList.add('inactive');
     } else {
-        burger.innerHTML = '<span style="font-size: 40px;">&times;</span>';
+        burger.innerHTML = '<span style="font-size: 60px;">&times;</span>';
         navbar.classList.remove('inactive');
         navbar.classList.add('active');
     }
