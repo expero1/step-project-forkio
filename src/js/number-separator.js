44eb6b6b5bc0980bd68b903ddd7c8c441bc08c4b
eugene <eu.emets@gmail.com>
2023-02-24 07:47:25 +0200

    remove commented code

--- src/js/number-separator.js
+++ src/js/number-separator.js
@@ -2,10 +2,3 @@ let numSepartor = document.querySelectorAll(".num-separator");
 numSepartor.forEach((num) => {
   num.innerText = Number(num.innerText).toLocaleString("en");
 });
-
-// for (let i = 0, len = x.length; i < len; i++) {
-//     let num = Number(x[i].innerHTML)
-//             .toLocaleString('en');
-//     x[i].innerHTML = num;
-//     x[i].classList.add("currSign");
-// }
